2fa5ef56dba16e1e4df82e9ba1e35cdc6a7f88d6
Ho Minh Quang <quang0805kk@gmail.com>
2025-04-21 18:47:40 +0700

    firebase: add auth config

---
+++ app/src/main/java/com/example/projectiiapp/AuthState.kt
@@ -0,0 +1,9 @@
+package com.example.projectiiapp
+
+sealed class AuthState {
+    object Idle: AuthState()
+    object Loading: AuthState()
+    data class Success(val userId: String): AuthState()
+    data class Error(val message: String): AuthState()
+    object LoggedOut: AuthState()
+}
