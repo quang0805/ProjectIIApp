d301a9d0e3e4f516d63547b5bef06eb7b5083d2a
hason123 <52872396+hason123@users.noreply.github.com>
2025-05-20 00:35:48 +0700

    Add files via upload

--- Project2.ino
+++ Project2.ino
@@ -2,22 +2,24 @@
 #include<PubSubClient.h>
 #include<DHT.h>
 #include <WifiClientSecure.h>
+#include <WiFiManager.h>
+
 //thay bang ten, mat khau wifi cua minh
-const char* ssid = "AndroidAP";
-const char* password = "12345678";
+////const char* ssid = "AndroidAP";
+//const char* password = "12345678";
 
 
-//const char* mqttServer = "7249966839ac4bf68fc9bb228451bd0b.s1.eu.hivemq.cloud";
-//const int mqttPort = 8883;                    
-//const char* mqttClientID = "ESP8266_DHT11";   
-//const char* mqttUser = "quang";       
-//const char* mqttPassword = "Quangkk123";  
-
-const char* mqttServer = "7882f49ec5a24abc9c49b6c8332f73e4.s1.eu.hivemq.cloud";
+const char* mqttServer = "7249966839ac4bf68fc9bb228451bd0b.s1.eu.hivemq.cloud";
 const int mqttPort = 8883;                    
 const char* mqttClientID = "ESP8266_DHT11";   
-const char* mqttUser = "hayson";       
-const char* mqttPassword = "Alo123,./";  
+const char* mqttUser = "quang";       
+const char* mqttPassword = "Quangkk123";  
+
+//const char* mqttServer = "7882f49ec5a24abc9c49b6c8332f73e4.s1.eu.hivemq.cloud";
+//const int mqttPort = 8883;                    
+//const char* mqttClientID = "ESP8266_DHT11";   
+//const char* mqttUser = "hayson";       
+//const char* mqttPassword = "Alo123,./";  
 
 // Khai báo cảm biến DHT11
 #define LED D7
@@ -25,6 +27,14 @@ const char* mqttPassword = "Alo123,./";
 #define DHTPIN D4      
 #define DHTTYPE DHT11  
 DHT dht(DHTPIN, DHTTYPE);
+
+#define AOUT_PIN A0  
+
+int soilMoistureValue = 0;
+int soilmoisturepercent=0;
+
+const int AirValue = 800;   //you need to replace this value with Value_1
+const int WaterValue = 370;
 
 // Khởi tạo WiFi và MQTT Client
 WiFiClientSecure espClient;
@@ -36,13 +46,25 @@ char message[MSG_BUFFER_SIZE];
 
 // Hàm kết nối WiFi
 void setup_wifi() {
-  Serial.print("Đang kết nối WiFi...");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi đã kết nối!");
+  //Serial.print("Đang kết nối WiFi...");
+  //WiFi.begin(ssid, password);
+  //while (WiFi.status() != WL_CONNECTED) {
+   // delay(500);
+   // Serial.print(".");
+  //}
+  //Serial.println("\nWiFi đã kết nối!");
+  WiFi.mode(WIFI_STA);
+  WiFiManager vm;
+  bool res;
+  res = vm.autoConnect("Project2");
+  if(!res) {
+        Serial.println("Failed to connect");
+        ESP.restart();
+    } 
+    else {
+        //if you get here you have connected to the WiFi    
+        Serial.println("connected...yeey :)");
+    }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -116,24 +138,40 @@ void loop() {
     reconnect_mqtt();
   }
   client.loop(); 
+  unsigned long now = millis();
 
-  if (millis() - lastPublishTime >= 5000) {
-   lastPublishTime = millis();  
+  if (now - lastPublishTime >= 3000) {
+    lastPublishTime = now;
+    soilMoistureValue = analogRead(A0);  
+    soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+    soilmoisturepercent = constrain(soilmoisturepercent, 0, 100);  // Giới hạn từ 0 đến 100
 
+    // Đọc cảm biến DHT
     float temperature = dht.readTemperature(); 
-    float humidity = dht.readHumidity();       
+    float humidity = dht.readHumidity();  
 
+    
+    Serial.print("Độ ẩm đất: ");
+    Serial.print(soilmoisturepercent);
+    Serial.print(" % | Nhiệt độ: ");
+    Serial.print(temperature);
+    Serial.print("°C | Độ ẩm không khí: ");
+    Serial.print(humidity);
+    Serial.println("%");
+
+    String soilPayload = "{\"Độ ẩm đất\": " + String(soilmoisturepercent) + "}";
+    client.publish("soil/data", soilPayload.c_str());
+    
     if (!isnan(temperature) && !isnan(humidity)) {
-      Serial.print("Nhiệt độ: ");
-      Serial.print(temperature);
-      Serial.print("°C | Độ ẩm: ");
-      Serial.print(humidity);
-      Serial.println("%");
+      // Gửi dữ liệu cảm biến đất
+      
 
-      String payload = "{\"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + "}";
-      client.publish("sensor/data", payload.c_str());
+      // Gửi dữ liệu DHT
+      String dhtPayload = "{\"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + "}";
+      client.publish("sensor/data", dhtPayload.c_str());
+    } else {
+      Serial.println("Lỗi đọc cảm biến DHT");
     }
 
   }
-  
 }
